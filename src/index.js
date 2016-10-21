56692874b13fa359e5375faaa3e5a63b62ef8f2f
Andy Walpole <andywalpole@Andys-iMac.local>
2016-10-21 15:29:37 +0100

    added VideoList component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
 // Create new component. This component should produce some HTML
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSeach from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyCfoOI345aqeJ-_VBf3Cqb9mnGjDCCZCuA';
 
-const APP = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	);
-};
+
+class APP extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {videos: []};
+
+		YTSeach({key: API_KEY, term: 'surfboards'}, videos => {
+			this.setState({videos});
+		});
+
+	}
+
+	render() {
+
+		return (
+			<div>
+				<SearchBar />
+				<VideoList videos={this.state.videos} />
+			</div>
+		);
+
+	}
+
+}
 
 // Take this component generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<APP />, document.querySelector('.container'));
