73cfc2072492e856082d33bfcfdd1931f16d642a
Andy Walpole <andywalpole@Andys-iMac.local>
2016-10-21 14:20:15 +0100

    added event to search bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,14 +2,24 @@ import React, {Component} from 'react';
 
 class SearchBar extends Component {
 
-	render() {
-		return <input onChange={this.onInputChange} />;
+	constructor(props) {
+		super(props);
+		this.state = {term: 'yes'}
 	}
 
-	// event handler
-	onInputChange(event) {
-		console.dir(event.target.value);
+	render() {
+		// added value like value={this.state.term} creates a controlled component
+		return (
+			<div>
+				<input
+					value={this.state.term}
+					onChange={event => this.setState({term: event.target.value})}/><br/>
+				Value of the input: {this.state.term}<br/>
+			</div>
+		);
+
 	}
+
 
 }
 
